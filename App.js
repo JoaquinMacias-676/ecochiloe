68c51eb5e866ad4256ca9ed39360c0f8373c465c
Joaquin Macias <omermacias@gmail.com>
2025-06-15 03:06:08 -0400

    Probando botones

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button, Alert} from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable, Button, Alert} from 'react-native';
 
 const logo = require('./assets/ecochiloe_logo.png');
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.container}> 
       <Image source={logo} style={{ 
         widht: 200, 
         height: 200,
@@ -18,10 +18,14 @@ export default function App() {
       }}
       >EcoChiloé</Text>
       <StatusBar style="auto" />
-      <Button 
-        title='Inicie sesión'
-        color="#000" 
-        onPress={() => Alert.alert('¡Alerta!', 'Este es un mensaje')}/>
+      <Pressable
+        onPress={() => Alert.alert('¡Alerta!', 'Este es un mensaje')}
+        style={({ pressed }) => [
+          { padding: 10, borderRadius: 5, backgroundColor: pressed ? '#cce7cc' : '#39b54'}
+        ]}
+      >
+        <Text style={{ color: 'fff', fontWeight: '600' }}>Presiona Aquí</Text>
+      </Pressable>
     </View>
   );
 }
