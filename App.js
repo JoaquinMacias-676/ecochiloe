1a0a7b62c6e08bd57082b98c3e29515ef1133a16
Joaquin Macias <omermacias@gmail.com>
2025-06-15 02:06:31 -0400

    Probando diferentes funciones

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert} from 'react-native';
 
-const logo = require('./assets/logo_ecochiloe.png');
+const logo = require('./assets/ecochiloe_logo.png');
 
 export default function App() {
   return (
@@ -11,8 +11,17 @@ export default function App() {
         height: 200,
         resizeMode: 'center',
         }} />
+      <Text style={{
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold'
+      }}
+      >EcoChiloé</Text>
       <StatusBar style="auto" />
-      <Text>Tonto el que lea</Text>
+      <Button 
+        title='Inicie sesión'
+        color="#000" 
+        onPress={() => Alert.alert('¡Alerta!', 'Este es un mensaje')}/>
     </View>
   );
 }
