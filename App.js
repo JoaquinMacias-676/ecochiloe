994d76dd5981b67faae3c8b51752e5a58f572cd7
Joaquin Macias <omermacias@gmail.com>
2025-06-17 01:29:12 -0400

    Optimización del código del home (mejor orden)

--- App.js
+++ App.js
@@ -1,50 +1,86 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Pressable, Button, Alert} from 'react-native';
-
-const logo = require('./assets/ecochiloe_logo.png');
+import { StyleSheet, Text, View, Image, Pressable, Button} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}> 
-      <Text style={{
-        color: 'white',
-        fontSize: 20,
-        fontWeight: 'center'
-      }}
-      >Mis Cultivos</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}>
+      <Text style={styles.textocolor}>Mis Cultivos</Text><StatusBar style="auto" />
+      <Text style={styles.textocolor_contador}>0/3</Text>
       <Pressable
-        style={({ pressed }) => [
-          { marginVertical: 10, padding: 50, borderRadius: 16, borderWidth: 2, bordercolor: '#0c7744', backgroundColor: pressed ? '#0c7744' : '#78e0af'}
-        ]}
-      >
-        <Text style={{ color: '#fff', fontWeight: '800' }}>¡Agrega un Cultivo!</Text>
+        style={({ pressed }) => [{ 
+          marginVertical: 16, 
+          padding: 50, 
+          borderRadius: 16, 
+          borderWidth: 2, 
+          bordercolor: '#0c7744', 
+          backgroundColor: pressed ? '#0c7744' : '#78e0af'
+        }]}>
+        <Text style={styles.textocolor_botones}>¡Agrega un Cultivo!</Text>
       </Pressable>
 
       <Pressable
-        style={({ pressed }) => [
-          { marginVertical: 10, padding: 50, borderRadius: 16, borderWidth: 2, bordercolor: '#0c7744', backgroundColor: pressed ? '#0c7744' : '#78e0af'}
-        ]}
-      >
-        <Text style={{ color: '#fff', fontWeight: '800' }}>¡Agrega un Cultivo!</Text>
+        style={({ pressed }) => [{ 
+          marginVertical: 16, 
+          padding: 50, 
+          borderRadius: 16, 
+          borderWidth: 2, 
+          bordercolor: '#0c7744', 
+          backgroundColor: pressed ? '#0c7744' : '#78e0af'
+        }]}>
+        <Text style={styles.textocolor_botones}>¡Agrega un Cultivo!</Text>
       </Pressable>
 
       <Pressable
-        style={({ pressed }) => [
-          { marginVertical: 10, padding: 50, borderRadius: 16, borderWidth: 2, bordercolor: '#0c7744', backgroundColor: pressed ? '#0c7744' : '#78e0af'}
-        ]}
-      >
-        <Text style={{ color: '#fff', fontWeight: '800' }}>¡Agrega un Cultivo!</Text>
+        style={({ pressed }) => [{ 
+          marginVertical: 16, 
+          padding: 50, 
+          borderRadius: 16, 
+          borderWidth: 2, 
+          bordercolor: '#0c7744', 
+          backgroundColor: pressed ? '#0c7744' : '#78e0af'
+        }]}>
+        <Text style={styles.textocolor_botones}>¡Agrega un Cultivo!</Text>
       </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  
+// Estilo del fondo
   container: {
     flex: 1,
     backgroundColor: '#00bf63',
     alignItems: 'center',
-    paddingTop: 150
+    paddingTop: 150,
+  },
+
+// Estilo para el título  
+  textocolor: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    textShadowColor: 'black',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 1,
+  },
+
+// Estilo para el contador de cultivos
+  textocolor_contador: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+    textShadowColor: 'black',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 1,
+  },  
+// Estilo para el texto de los botones
+  textocolor_botones: {
+    fontSize: 15,
+    color: 'white',
+    fontWeight: 'bold',
+    textShadowColor: 'black',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 1,
   },
 });
