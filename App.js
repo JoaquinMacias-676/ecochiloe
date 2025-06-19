9a3bafb9c32f6b1bdbfb165d3f6ceeda2cf466dc
Joaquin Macias <omermacias@gmail.com>
2025-06-19 08:59:54 -0400

    Creando un prototipo de Login

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator} from '@react-navigation/native-stack';
-import Pantallainicio from './Pantallainicio.js';
+import LoginScreen from './LoginScreen';
 import HomeScreen from './HomeScreen';
 import CultivosScreen from './CultivosScreen';
 
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Inicio" component={Pantallainicio} />
+        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Cultivos" component={CultivosScreen} />
       </Stack.Navigator>
---
+++ LoginScreen.js
@@ -0,0 +1,63 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Pressable, Image } from 'react-native';
+
+export default function Login({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('./assets/ecochiloe_3.png')}
+        style={styles.logo_estilo}
+        />
+      <Pressable
+        onPress={() => navigation.navigate('Home')}
+        style={({ pressed }) => [ 
+            styles.botones,
+            { backgroundColor: pressed ? '#0c7744' : '#78e0af' }
+        ]}>
+        <Text style={styles.textocolor_botones}>Ingresar</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+// Creando los diferentes estilos
+const styles = StyleSheet.create({
+  
+// Estilo del fondo
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    paddingTop: 150,
+  },
+
+// Estilo para el texto de los botones
+  textocolor_botones: {
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: 'bold',
+    textShadowColor: '#00000',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 1,
+  },
+
+// Estilo para los botones
+  botones: {
+    marginVertical: 150, 
+    width: 150,
+    height: 75, 
+    borderRadius: 16, 
+    borderWidth: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#0c7744', 
+  },
+
+// Estilo Logo
+  logo_estilo: {
+    width: 250,
+    height: 250,
+    resizeMode: 'contain',
+
+  }
+});
