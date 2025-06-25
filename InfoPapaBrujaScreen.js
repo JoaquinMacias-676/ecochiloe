903199e4782e2a73169cbd2b039ba2aae7c85184
Joaquin Macias <omermacias@gmail.com>
2025-06-25 13:51:45 -0400

    creando interfaz de papa bruja

---
+++ InfoPapaBrujaScreen.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View, Pressable, Image } from 'react-native';
+import { styles } from './styles';
+
+export default function InfoPapaBruja({ navigation }) {
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+}
