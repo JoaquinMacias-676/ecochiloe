2b22516a2b9b21a61ce0c2323482cda0d256df93
Joaquin Macias <omermacias@gmail.com>
2025-06-26 02:17:53 -0400

    Cambiando estilo botones y quitando la importancias de StyleSheet

--- BusCultivosScreen.js
+++ BusCultivosScreen.js
@@ -1,4 +1,5 @@
-import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { Text, View, Pressable } from 'react-native';
+import { styles } from './styles';
 
 export default function BusquedaCultivos({ navigation }) {
   return (
@@ -39,36 +40,3 @@ export default function BusquedaCultivos({ navigation }) {
     </View>
   );
 }
-
-// Creando los diferentes estilos
-const styles = StyleSheet.create({
-  
-// Estilo del fondo
-  container: {
-    flex: 1,
-    backgroundColor: '#00bf63',
-    alignItems: 'center',
-    paddingTop: 150,
-  },
-
-// Estilo para el texto de los botones
-  textocolor_botones: {
-    fontSize: 15,
-    color: 'white',
-    fontWeight: 'bold',
-    textShadowColor: 'black',
-    textShadowOffset: { width: -1, height: 1 },
-    textShadowRadius: 1,
-  },
-// Estilo para los botones
-  botones: {
-    marginVertical: 10, 
-    width: 200,
-    height: 150, 
-    borderRadius: 16, 
-    borderWidth: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#0c7744', 
-  },
-});
--- InfoPapaBrujaScreen.js
+++ InfoPapaBrujaScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Pressable, Image } from 'react-native';
+import { Text, View, Pressable, Image } from 'react-native';
 import { styles } from './styles';
 
 export default function InfoPapaBruja({ navigation }) {
