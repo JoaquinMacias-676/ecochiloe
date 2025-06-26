b4d462b12a0b36a5dcf0fd51c2563f63dd387602
Joaquin Macias <omermacias@gmail.com>
2025-06-26 01:47:09 -0400

    Agregando iconos *provisionales* y botones interactivos en las interfaces "Cereales", "Frutas" y "Hortalizas"

--- CerealesScreen.js
+++ CerealesScreen.js
@@ -1,49 +1,63 @@
-import { StyleSheet, Text, View, Pressable, Image } from 'react-native';
+import { Text, View, Pressable, Image } from 'react-native';
+import { styles } from './styles';
 
 export default function Cereales({ navigation }) {
     return (
         <View style={styles.container}>
+          <Pressable
+            onPress={() => navigation.navigate('SeleccionCultivo')}
+            style={styles.flechaboton}>
+            <Image
+                source={require('./assets/flecha_atras.png')}
+                style={[styles.flechaimagen, { tintColor: '#000000'}]}/>
+          </Pressable>
+            <View style={styles.botones_horizontal_cultivos}>
             <Pressable
-                onPress={() => navigation.navigate('Home')}
+                onPress={() => navigation.navigate('')}
                 style={({ pressed }) => [ 
-                    styles.botones,
-                    { backgroundColor: pressed ? '#0c7744' : '#78e0af', marginTop: 50 }
-                ]}>
-            </Pressable> 
+                    styles.botones_clasificacion_cultivos,
+                    { backgroundColor: pressed ? '#0c7744' : '#78e0af', marginTop: 50 }]}>
+                <Text style={styles.papas}>???</Text>
+                <Image 
+                    source={require('./assets/ecochiloe_logo.png')}
+                    style={styles.cultivos_iconos} />
+            </Pressable>
+
+            <Pressable
+                onPress={() => navigation.navigate('')}
+                style={({ pressed }) => [ 
+                    styles.botones_clasificacion_cultivos,
+                    { backgroundColor: pressed ? '#0c7744' : '#78e0af', marginTop: 50 }]}>
+                <Text style={styles.papas}>???</Text>
+                <Image
+                    source={require('./assets/ecochiloe_logo.png')}
+                    style={styles.cultivos_iconos} />
+            </Pressable>
+            </View>
+
+            <View style={styles.botones_horizontal_cultivos}>
+            <Pressable
+                onPress={() => navigation.navigate('')}
+                style={({ pressed }) => [ 
+                    styles.botones_clasificacion_cultivos,
+                    { backgroundColor: pressed ? '#0c7744' : '#78e0af', marginTop: 50 }]}>
+                <Text style={styles.papas}>???</Text>
+                <Image
+                    source={require('./assets/ecochiloe_logo.png')}
+                    style={styles.cultivos_iconos} />
+            </Pressable>
+
+            <Pressable
+                onPress={() => navigation.navigate('')}
+                style={({ pressed }) => [ 
+                    styles.botones_clasificacion_cultivos,
+                    { backgroundColor: pressed ? '#0c7744' : '#78e0af', marginTop: 50 }]}>
+                <Text style={styles.papas}>???</Text>
+                <Image
+                    source={require('./assets/ecochiloe_logo.png')} 
+                    style={styles.cultivos_iconos} />
+            </Pressable>
+            </View>  
         </View>
-    );
-}
-
-// Creando los diferentes estilos
-const styles = StyleSheet.create({
-  
-// Estilo del fondo
-  container: {
-    flex: 1,
-    backgroundColor: '#00bf63',
-    alignItems: 'center',
-    paddingTop: 150,
-  },
-
-// Estilo para el texto de los botones
-  textocolor_botones: {
-    fontSize: 15,
-    color: 'white',
-    fontWeight: 'bold',
-    textShadowColor: 'black',
-    textShadowOffset: { width: -1, height: 1 },
-    textShadowRadius: 1,
-  },
-
-// Estilo para los botones
-  botones: {
-    marginVertical: 10, 
-    width: 250,
-    height: 85, 
-    borderRadius: 16, 
-    borderWidth: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#0c7744', 
-  },
-});
+      );
+    }
