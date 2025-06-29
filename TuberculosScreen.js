1662152703f5fd9d9eefc785e9b96813abd679ec
Joaquin Macias <omermacias@gmail.com>
2025-06-28 23:58:26 -0400

    añadiendo título de interfaz

--- TuberculosScreen.js
+++ TuberculosScreen.js
@@ -4,14 +4,17 @@ import { styles } from './styles';
 export default function Tuberculos({ navigation }) {
     return (
         <View style={styles.container}>
-          
-          <Pressable
-            onPress={() => navigation.navigate('SeleccionCultivo')}
-            style={styles.flechaboton}>
+
+            <Text style={styles.titulo_interfaz}>TUBÉRCULOS</Text>
+
+            <Pressable
+                onPress={() => navigation.navigate('SeleccionCultivo')}
+                style={styles.flechaboton}>
             <Image
                 source={require('./assets/flecha_atras.png')}
                 style={[styles.flechaimagen, { tintColor: '#000000'}]}/>
-          </Pressable>
+            </Pressable>
+
             <View style={styles.botones_horizontal_cultivos}>
             <Pressable
                 onPress={() => navigation.navigate('InfoPapaBruja')}
@@ -38,28 +41,28 @@ export default function Tuberculos({ navigation }) {
             </View>
 
             <View style={styles.botones_horizontal_cultivos}>
-            <Pressable
-                onPress={() => navigation.navigate('')}
-                style={({ pressed }) => [ 
-                    styles.botones_clasificacion_cultivos,
-                    { backgroundColor: pressed ? '#0c7744' : '#78e0af', marginTop: 50 }]}>
-                <Text style={styles.papas}>Papa Murta Ojuda</Text>
-                <Image
-                    source={require('./assets/papa_murta_ojuda.png')}
-                    style={styles.cultivos_iconos} />
-            </Pressable>
+                <Pressable
+                    onPress={() => navigation.navigate('')}
+                    style={({ pressed }) => [ 
+                        styles.botones_clasificacion_cultivos,
+                        { backgroundColor: pressed ? '#0c7744' : '#78e0af', marginTop: 50 }]}>
+                    <Text style={styles.papas}>Papa Murta Ojuda</Text>
+                    <Image
+                        source={require('./assets/papa_murta_ojuda.png')}
+                        style={styles.cultivos_iconos} />
+                </Pressable>
 
-            <Pressable
-                onPress={() => navigation.navigate('')}
-                style={({ pressed }) => [ 
-                    styles.botones_clasificacion_cultivos,
-                    { backgroundColor: pressed ? '#0c7744' : '#78e0af', marginTop: 50 }]}>
-                <Text style={styles.papas}>Papa Cacho de Toro</Text>
+                <Pressable
+                    onPress={() => navigation.navigate('')}
+                    style={({ pressed }) => [ 
+                        styles.botones_clasificacion_cultivos,
+                        { backgroundColor: pressed ? '#0c7744' : '#78e0af', marginTop: 50 }]}>
+                    <Text style={styles.papas}>Papa Cacho de Toro</Text>
                 <Image
                     source={require('./assets/papa_cacho_de_toro.png')} 
                     style={styles.cultivos_iconos} />
             </Pressable>
             </View>  
         </View>
-      );
-    }
+    );
+}
