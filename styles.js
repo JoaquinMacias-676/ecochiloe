e76b66a1f0b9992d1af59d29fd31de0c39657d23
Joaquin Macias <omermacias@gmail.com>
2025-07-05 02:52:31 -0400

    Quitando estilos del login

--- styles.js
+++ styles.js
@@ -16,15 +16,7 @@ export const styles = StyleSheet.create({
     marginBottom: 20,
     marginRight: 150,
   },
-
-// Estilo del fondo blanco
-  container_blanco: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    paddingTop: 150,
-  },
-
+  
 // Estilo para el título que describe la pantalla
   titulo_interfaz: {
     fontSize: 30,
@@ -36,15 +28,6 @@ export const styles = StyleSheet.create({
     textShadowColor: 'black',
     textShadowOffset: { width: 0, height: 0 },
     textShadowRadius: 6,
-  },
-
-// Estulo para el texto de bienvenida
-  bienvenida_texto: {
-    marginVertical: -30,
-    fontSize: 22,
-    color: '#00000',
-    fontFamily: 'Roboto Condensed Bold',
-    fontWeight: '900',
   },
 
 // Estilo para el título "Mis Cultivos"
@@ -77,18 +60,6 @@ export const styles = StyleSheet.create({
     textShadowOffset: { width: 0, height: 0 },
     textShadowRadius: 6,
   },  
-
-// Estilo para los botones
-  botones_login: {
-    marginVertical: 180, 
-    width: 150,
-    height: 60, 
-    borderRadius: 16, 
-    borderWidth: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#0c7744', 
-  },
 
 // Estilo para los botones de la pantalla principal
   botones_principal: {
@@ -160,13 +131,5 @@ export const styles = StyleSheet.create({
     textShadowOffset: { width: 0, height: 0 },
     textShadowRadius: 6,
     textAlign: 'center',
-  },
-
-// Estilo Logo Login
-  logo_estilo: {
-    marginVertical: -50,
-    width: 275,
-    height: 275,
-    resizeMode: 'contain',
   },
 });
