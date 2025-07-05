ce363ec1bdb3beed2e5e0aa263b47f176c75d7bd
Joaquin Macias <omermacias@gmail.com>
2025-07-05 19:37:52 -0400

    Quitando estilos del login antiguo

--- styles_login.js
+++ styles_login.js
@@ -9,6 +9,14 @@ export const styles = StyleSheet.create({
     paddingTop: 70,
   },
 
+// Estilo Logo Login
+  logo_estilo: {
+    marginVertical: -50,
+    width: 260,
+    height: 260,
+    resizeMode: 'contain',
+  },
+
 // Botones de login
   botones: {
     width: 330,
@@ -19,44 +27,6 @@ export const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     borderColor: '#ebebeb',
-  },
-// Estilo para el texto del login
-  textobotones_login: {
-    fontSize: 20,
-    color: 'white',
-    fontFamily: 'Baloo 2 Bold',
-    textShadowColor: 'black',
-    textShadowOffset: { width: 0, height: 0 },
-    textShadowRadius: 6,
-  },
-
-// Estilo para el texto de bienvenida
-  bienvenida_texto: {
-    marginVertical: -30,
-    fontSize: 22,
-    color: '#00000',
-    fontFamily: 'Roboto Condensed Bold',
-    fontWeight: '900',
-  },
-
-// Estilo para los botones
-  botones_login: {
-    marginVertical: 180, 
-    width: 150,
-    height: 60, 
-    borderRadius: 16, 
-    borderWidth: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#0c7744', 
-  },
-
-// Estilo Logo Login
-  logo_estilo: {
-    marginVertical: -50,
-    width: 260,
-    height: 260,
-    resizeMode: 'contain',
   },
 
 // Estilo para el cuadro de "Inicia de sesión"
