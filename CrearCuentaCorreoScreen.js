70236e868f2f6ad3acd3c29b122833756b4234cf
Joaquin Macias <omermacias@gmail.com>
2025-07-05 18:29:22 -0400

    Avanzando la interfaz de creación de cuenta (falta hacer funcional los botones)

--- CrearCuentaCorreoScreen.js
+++ CrearCuentaCorreoScreen.js
@@ -1,11 +1,12 @@
 import { Text, View, Pressable, Image, ActivityIndicator } from 'react-native';
 import { styles } from './styles_login';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { useFonts } from 'expo-font';
 
 export default function CrearCuentaCorreo({ navigation }) {
   const [fontsLoaded] = useFonts({
-    'Roboto Condensed Bold': require('./assets/fonts/Roboto_Condensed-Bold.ttf'),
-    'Roboto Light': require('./assets/fonts/Roboto-Light.ttf'),
+    'Roboto Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+    'Roboto Regular': require('./assets/fonts/Roboto-Regular.ttf'),
   });
 
   if (!fontsLoaded) {
@@ -22,13 +23,31 @@ export default function CrearCuentaCorreo({ navigation }) {
         <Image
           source={require('./assets/ecochiloe_3.png')}
           style={styles.logo_estilo} />
-        <Pressable
-        onPress={() => navigation.navigate('Login')}
-        style={({ pressed }) => [ 
-            styles.botones_login,
-            { backgroundColor: pressed ? '#0c7744' : '#78e0af' }]}>
-        <Text style={styles.textobotones_login}>boton de prueba</Text>
+
+        <Text style={styles.texto_grande}>Crea una cuenta</Text>
+
+        <Text style={styles.texto_normal}>Por medio de un correo electrónico</Text>
+
+        <Pressable style={[styles.botones, { marginTop: 20, flexDirection: 'row' }]}>
+          <Icon name='envelope' size={24} color='#ebebeb' />
+          <Text style={[styles.texto_normal, { paddingHorizontal: 40 }]}>Ingrese un correo electrónico</Text>
         </Pressable>
+
+        <Pressable style={[styles.botones, { backgroundColor: '#000', borderColor: '#000'}]}
+        onPress={() => navigation.navigate('Home')}>
+          <Text style={[styles.texto_normal, { color: '#fff'}]}>Continuar</Text> 
+        </Pressable>
+
+        <View style={styles.container_gris}>
+
+          <Text style={styles.texto_normal}>¿Ya tienes una cuenta?{' '}
+            <Text style={[styles.texto_normal, { color: '#00bf63', textDecorationLine: 'underline', }]}
+            onPress={() => navigation.navigate('Home')}>
+              Inicia sesión</Text>
+          </Text>
+
+        </View>
+
     </View>
   );
 }
