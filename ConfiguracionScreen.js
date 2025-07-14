3c6cad3091d0b5167fac13fe53921fbc6b876066
Joaquin Macias <omermacias@gmail.com>
2025-07-13 20:30:43 -0400

    Creando nuevo archivo de configuración

---
+++ ConfiguracionScreen.js
@@ -0,0 +1,10 @@
+import { Text, View, Pressable, Image } from 'react-native';
+import { styles } from './styles';
+
+export default function Configuracion({ navigation }) {
+    return (
+        <View style={styles.container}>
+            
+        </View>
+    );
+}
