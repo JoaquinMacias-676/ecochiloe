814c0fd17b21ba99eeea9cb3291d147b7540c302
Joaquin Macias <omermacias@gmail.com>
2025-07-14 01:18:37 -0400

    Agregando botón para eliminar todos los datos del json

--- ConfiguracionScreen.js
+++ ConfiguracionScreen.js
@@ -1,7 +1,8 @@
-import { View, Text, Pressable } from 'react-native';
+import { View, Text, Pressable, Alert } from 'react-native';
 import { styles } from './styles';
-
 import { FontAwesome5 } from '@expo/vector-icons';
+
+import * as FileSystem from 'expo-file-system';
 
 export default function Configuracion({ navigation }) {
     const cerrarSesion = () => {
@@ -11,14 +12,39 @@ export default function Configuracion({ navigation }) {
     });
   };
 
+// Para eliminar todos los datos del json
+const eliminarTodosLosDatos = async () => {
+  try {
+    const fileUri = FileSystem.documentDirectory + 'cuentas.json';
+    
+    const fileExists = await FileSystem.getInfoAsync(fileUri);
+    if (!fileExists.exists) {
+      Alert.alert('Aviso', 'El archivo ya está vacío o no existe.');
+      return;
+    }
+
+    await FileSystem.writeAsStringAsync(fileUri, JSON.stringify([]));
+    Alert.alert('Éxito', 'Todos los datos han sido eliminados.');
+  } catch (error) {
+    console.error('Error al eliminar los datos:', error);
+    Alert.alert('Error', 'Ocurrió un error al intentar eliminar los datos.');
+  }
+};
+
   return (
     <View style={styles.container}>
         <Text style={[styles.textocolor, { paddingBottom: 120 }]}>CONFIGURACIÓN</Text>
         <Pressable
-            style={[styles.boton_configuracion, { borderBottomWidth: 4, borderBottomColor: '#0c7744' }]}
+            style={styles.boton_configuracion}
             onPress={cerrarSesion}>
-            <FontAwesome5 name= 'sign-out-alt' size={25} color='white' solid style={{ paddingLeft: 20}} />
+            <FontAwesome5 name='sign-out-alt' size={25} color='white' style={{ paddingLeft: 20}} />
             <Text style={styles.texto_configuracion}>Cerrar sesión</Text>
+        </Pressable>
+        <Pressable
+          style={[styles.boton_configuracion, { borderBottomWidth: 4, borderBottomColor: '#0c7744' }]}
+          onPress={eliminarTodosLosDatos}>
+          <FontAwesome5 name='exclamation-triangle' size={23} color='white' style={{ paddingLeft: 20 }} />
+          <Text style={styles.texto_configuracion}>Eliminar todos los datos</Text>
         </Pressable>
     </View>
   );
