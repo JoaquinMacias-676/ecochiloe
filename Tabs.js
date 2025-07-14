4cdbc2e84aa608b3e1ac490ba8ae64e927a806e1
Joaquin Macias <omermacias@gmail.com>
2025-07-13 21:38:44 -0400

    Mejorando el Tab

--- Tabs.js
+++ Tabs.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './HomeScreen';
 import Configuracion from './ConfiguracionScreen';
@@ -11,14 +12,20 @@ export default function Tabs() {
   return (
     <Tab.Navigator
       screenOptions={({ route }) => ({
-        tabBarActiveTintColor: '#00bf63',
-        tabBarInactiveTintColor: '#888',
-        tabBarStyle: {
-          backgroundColor: '#ffffff',
-          borderTopLeftRadius: 15,
-          borderTopRightRadius: 15,
-          height: 100,
+        tabBarActiveTintColor: 'white',
+        tabBarInactiveTintColor: 'black',
+        tabBarLabelStyle: {
+            fontFamily: 'Baloo 2 Bold',
+            fontSize: 10,
+            marginTop: 10,
         },
+        tabBarStyle: {
+          backgroundColor: 'transparent',
+          height: 120,
+          borderTopWidth: 0,
+          elevation: 0,
+        },
+        tabBarBackground: () => <View style={styles.tab_borde} />,
         headerShown: false,
         tabBarIcon: ({ color, size }) => {
           let iconName;
@@ -29,12 +36,31 @@ export default function Tabs() {
             iconName = 'cog';
           }
 
-          return <Icon name={iconName} size={size} color={color} />;
+          return (
+            <Icon 
+                name={iconName}
+                size={30}
+                color={color}
+                style={{ marginBottom: -20 }}
+            />
+            );
         },
       })}
     >
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Configuracion" component={Configuracion} />
+      <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'INICIO' }} />
+      <Tab.Screen name="Configuracion" component={Configuracion} options={{ title: 'CONFIGURACIÓN' }} />
     </Tab.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  tab_borde: {
+    flex: 1,
+    backgroundColor: '#0c7744',
+    borderTopWidth: 6,
+    borderTopColor: '#23a768',
+    borderStyle: 'solid',
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0,
+  },
+});
