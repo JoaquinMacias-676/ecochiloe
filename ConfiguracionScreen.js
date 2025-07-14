5134d3f5d74fa8c0afa87f1ac46851e4ae6ac05f
Joaquin Macias <omermacias@gmail.com>
2025-07-14 00:38:56 -0400

    Dejando la configuración con solo el cierre de sesión

--- ConfiguracionScreen.js
+++ ConfiguracionScreen.js
@@ -1,11 +1,9 @@
-import { View, Text, Pressable, TextInput } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 import { styles } from './styles';
-import { useState } from 'react';
 
 import { FontAwesome5 } from '@expo/vector-icons';
 
 export default function Configuracion({ navigation }) {
-    const [mostrarFormulario, setMostrarFormulario] = useState(false);
     const cerrarSesion = () => {
     navigation.reset({
       index: 0,
@@ -13,52 +11,9 @@ export default function Configuracion({ navigation }) {
     });
   };
 
-  const toggleFormulario = () => {
-    setMostrarFormulario(!mostrarFormulario);
-  };
-
   return (
     <View style={styles.container}>
         <Text style={[styles.textocolor, { paddingBottom: 100 }]}>CONFIGURACIÓN</Text>
-        <Pressable
-            style={styles.boton_configuracion}
-            onPress={toggleFormulario}>
-            <FontAwesome5
-            name={mostrarFormulario ? 'chevron-up' : 'chevron-down'}
-            size={25}
-            color="white"
-            solid
-            style={{ paddingLeft: 20 }}
-            />
-            <Text style={styles.texto_configuracion}>Modificar Contraseña</Text>
-        </Pressable>
-
-        {mostrarFormulario && (
-            <View style={{ padding: 20, width: '90%' }}>
-            <Text style={styles.label}>Contraseña actual</Text>
-            <TextInput
-                style={styles.input}
-                secureTextEntry
-                placeholder="Escribe tu contraseña actual"
-            />
-            <Text style={styles.label}>Nueva contraseña</Text>
-            <TextInput
-                style={styles.input}
-                secureTextEntry
-                placeholder="Escribe tu nueva contraseña"
-            />
-            <Text style={styles.label}>Confirmar nueva contraseña</Text>
-            <TextInput
-                style={styles.input}
-                secureTextEntry
-                placeholder="Confirma tu nueva contraseña"
-            />
-            <Pressable style={styles.boton_guardar}>
-                <Text style={styles.texto_guardar}>Guardar cambios</Text>
-            </Pressable>
-            </View>
-        )}
-
         <Pressable
             style={[styles.boton_configuracion, { borderBottomWidth: 4, borderBottomColor: '#0c7744' }]}
             onPress={cerrarSesion}>
